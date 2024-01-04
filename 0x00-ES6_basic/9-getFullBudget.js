d8ea415845517d5dc90a24f98594d18de095a83b
Sarah hassan <sarraree5@gmail.com>
2024-01-04 04:40:24 +0200

    tasks 6 - 12

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,13 @@
+import getBudgetObject from './7-getBudgetObject';
+// Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
