e40edc61d6580ffa46e7e9e55a6b431edb55c86d
Sarah Hassan <sarraree5@gmail.com>
2024-01-10 02:43:11 +0200

    advanced task

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,9 @@
+export const weakMap = new WeakMap();
+export const queryAPI = (endpoint) => {
+  if (weakMap.has(endpoint)) {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
+};
