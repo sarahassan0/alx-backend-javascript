7ff0dba5a39ca58394ad169aed90a947f26d4642
sarahassan0 <sarraree5@gmail.com>
2024-02-28 02:19:22 +0200

    feat: solve task 0, 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+const isPipedInput = !process.stdin.isTTY;
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
+if (isPipedInput) {
+  process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
